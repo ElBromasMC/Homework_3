28bc34fd5460ea56b9a18ffa2db560cda4dbf576
ElBromasMC <ander.linares@outlook.com>
2023-09-25 19:24:28 -0500

    Mamani ctm

--- exercises/ex_3b.py
+++ exercises/ex_3b.py
@@ -1,16 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Variables
-# Position of the particle
-a, b = 3, 2
-
-# Charge of the particle
-q = -3e-6
-
-
 # Constants
 K = 9e+9
+
+class Particle():
+    def __init__(self, position, charge):
+        self.position = position
+        self.charge = charge
+
+    def getXField(self, position):
+        return K*self.charge*(position[0]-self.position[0])/np.sqrt((position[0]-self.position[0])**2 + (position[1]-self.position[1])**2)**3
+    
+    def getYField(self, position):
+        return K*self.charge*(position[1]-self.position[1])/np.sqrt((position[0]-self.position[0])**2 + (position[1]-self.position[1])**2)**3
+
+
+# Define the particles
+particles = [ Particle((-1, -1), 3e-6)
+            , Particle((1, 1), 3e-6)
+            , Particle((1, -1), -3e-6)
+            , Particle((-1, 1), -3e-6)
+            ]
 
 
 
@@ -18,14 +29,18 @@ def ex_3a():
     # Domain of the vector field
     x, y = np.meshgrid(np.linspace(-5, +5, 30), np.linspace(-5, +5, 30))
     # The vector field
-    u, v = K*q*(x-a)/np.sqrt((x-a)**2 + (y-b)**2)**3, K*q*(y-b)/np.sqrt((x-a)**2 + (y-b)**2)**3
+    u, v = 0, 0
 
-    # Plot the particle
-    plt.plot(a, b, 'ro', markersize=20)
+    for particle in particles:
+        u += particle.getXField((x,y))
+        v += particle.getYField((x,y))
+
+        # Plot the particles
+        plt.plot(particle.position[0], particle.position[1], 'ro', markersize=20)
 
     # Plot the function via streamplot
     plt.streamplot(x, y, u, v)
-    plt.title('Ejercicio 3a')
+    plt.title('Ejercicio 3b')
     plt.grid(True)
     plt.gca().set_aspect('equal')
 
